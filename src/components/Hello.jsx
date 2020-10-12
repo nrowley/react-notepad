e057b316f102f3857eaabf724e58f756e2df3db9
nrowley <nelsonononivu2002@gmail.com>
2020-10-12 19:27:48 +0100

    add react-router

---
+++ src/components/Hello.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Hello() {
+  return <h2>it worked</h2>;
+}
---
+++ src/components/Topnav.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { VscRepo } from "react-icons/vsc";
+import { Link } from "react-router-dom";
+
+export default class Topnav extends Component {
+  render() {
+    return (
+      <Navbar className="navbar navbar-expand-lg navbar-fixed-top navbar-dark bg-dark">
+        <Navbar.Brand className="text-primary pink-hover lighten-2-hover">
+          <Link className="text-white text-decoration-none" to="#">
+            <VscRepo />
+            Notepad
+          </Link>
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link>
+            <Link className="text-secondary flex-end text-decoration-none" to="/login">
+              Login
+            </Link>
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+    );
+  }
+}
